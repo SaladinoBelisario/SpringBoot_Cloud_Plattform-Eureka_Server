8bc7455f735c1fa566ec69504cce1d8efee2aa9e
Saladino Belisario <SaladinoBelisario@protonmail.ch>
2022-10-19 15:17:16 -0500

    Init commit

---
+++ src/test/java/dev/saladinobelisario/springboot/app/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.saladinobelisario.springboot.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
